9c03b8c7ce59ec814d16bdcad164733eb4dd0b89
s <s>
2022-03-15 19:24:37 +0100

    Promjena

--- Vjezba_1/Program.cs
+++ Vjezba_1/Program.cs
@@ -6,7 +6,7 @@ namespace Vjezba_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
